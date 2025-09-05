577cb585e69793124cc4250025e53703aa725c90
Dinithi1625403 <dinithi1625403@gmail.com>
2025-09-05 09:30:40 +0530

    designed login and sign up

--- frontend/web/src/pages/LandingPage.js
+++ frontend/web/src/pages/LandingPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import LoginForm from '../components/LoginForm';
+
 
 const LandingPage = ({ onLogin }) => (
     <div className="landing-page">
         <h1>Personalized Learning Platform</h1>
-        <LoginForm onLogin={onLogin} />
+        
     </div>
 );
 
